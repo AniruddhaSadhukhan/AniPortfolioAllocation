49a56091a7b77741e9b0ef57a8e55dc7979a33bb
Aniruddha Sadhukhan <anisadhukhan1@gmail.com>
2026-08-31 19:20:53 +0530

    Angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from "@angular/core";
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from "@angular/core";
 
 import { environment } from "./environments/environment";
 
@@ -84,7 +84,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(
+    provideZoneChangeDetection(),importProvidersFrom(
       BrowserModule,
       AppRoutingModule,
       FormsModule,
